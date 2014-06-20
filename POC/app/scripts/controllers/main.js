c1d99dafdd8aea48b270393892fee24484012711
html5FJA <david.nguyen@captechconsulting.com>
2014-06-20 15:04:39 -0400

    update for mike

--- POC/app/scripts/controllers/main.js
+++ POC/app/scripts/controllers/main.js
@@ -9,7 +9,7 @@ angular.module('pocApp')
     ];
   });
 
-function MyController($scope, $http) {
+/*function MyController($scope, $http) {
     $scope.items = [];
     $scope.getItems = function() {
         $http({method : 'GET',url : 'http://192.168.89.15:8080/worldcup/v1/results'})
@@ -19,4 +19,46 @@ function MyController($scope, $http) {
             .error(function(data, status) {
                 alert("Error");
             });
-    }};
+    }};*/
+
+
+
+function MyController($scope, $http) {
+    $scope.items = [];
+    $scope.getResults = function() {
+	  	$http({method : 'GET',url : 'http://192.168.89.15:8080/worldcup/v1/results'})
+	  		.success(function(data, status) {
+	            $scope.items = data;
+	            data.reverse();
+	            })
+	            .error(function(data, status) {
+	                alert("Error");
+	            });
+	      	$scope.$apply();
+  	}
+
+  $scope.getSchedule = function() {
+	  	$http({method : 'GET',url : 'http://192.168.89.15:8080/worldcup/v1/schedule'})
+	  		.success(function(data, status) {
+	            $scope.items = data;
+	            })
+	            .error(function(data, status) {
+	                alert("Error");
+	            });
+	      $scope.$apply();
+  	}
+
+  	$scope.getLive = function() {
+	  	$http({method : 'GET',url : 'http://192.168.89.15:8080/worldcup/v1/live'})
+	  		.success(function(data, status) {
+	            $scope.items = data;
+	            data.reverse();
+	            })
+	            .error(function(data, status) {
+	                alert("Error");
+	            });
+	    $scope.$apply();
+  	}};
+
+
+  
